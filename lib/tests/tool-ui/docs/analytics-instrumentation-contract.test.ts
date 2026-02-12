9786533300f502219c8efcf3af6b2af103b07052
Pete Petrash <pete.petrash@gmail.com>
2026-02-12 08:13:34 -0800

    Track code_block_copied from component preview copy

--- lib/tests/tool-ui/docs/analytics-instrumentation-contract.test.ts
+++ lib/tests/tool-ui/docs/analytics-instrumentation-contract.test.ts
@@ -28,6 +28,7 @@ const FILE_ASSERTIONS = [
     needles: [
       "analytics.component.tabSwitched(",
       "analytics.component.previewInteracted(",
+      "analytics.code.blockCopied(",
     ],
   },
   {
